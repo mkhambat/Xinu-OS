9bf74e2ef70d9a05feba6af1c4a40e34871d4fa3
Murtaza Shabbir Khambaty <mkhambat@tank.soic.indiana.edu>
2018-04-04 14:43:33 -0400

    Assignment 5 futures part 2 with report

--- system/future_alloc.c
+++ system/future_alloc.c
@@ -8,6 +8,7 @@ future* future_alloc(int future_flags) 	//Allocates memory for a new future
 	new_future->state = FUTURE_EMPTY; 	//Set future's state to empty
 	new_future->flag = future_flags;	//Set future's flag to the input flag recieved
 	new_future->pid = NULL;				//Set pid as null
+	new_future->value = (int *)getmem(sizeof(int));
 	new_future->value = NULL;			//Set value as null
 	
 	return new_future;
